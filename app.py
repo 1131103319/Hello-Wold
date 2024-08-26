0823880a48e4be3dc20d8172a786ae843449d54e
隐藏玩家 <1131103319@qq.com>
2024-08-26 18:16:44 +0800

    add minimal home page

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User:{name}'
 @app.route('/test')
 def test_url_for():
-    print(url_for("hello"))
+    print(url_for("hello_world"))
     print(url_for("user_page", name='a'))
     print(url_for("user_page", name='b'))
     print(url_for("test_url_for", num=2))
     return 'test page'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
