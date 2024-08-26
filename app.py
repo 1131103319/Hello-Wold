95851d9fa7865122db92f0f8d6163dd42640b088
隐藏玩家 <1131103319@qq.com>
2024-08-26 22:10:17 +0800

    add database support with flask-sqlalchemy

--- app.py
+++ app.py
@@ -1,24 +1,57 @@
+import os
+
+import click
 from flask import Flask, url_for, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
-name='Mr.li'
-movies=[
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(app.root_path,'test.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db=SQLAlchemy(app)
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Delete database if exists')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+        click.echo('Dropped database')
+    elif None:
+        db.create_all()
+        click.echo('Initialized database')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(20))
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(60))
+    year=db.Column(db.String(4))
+@app.cli.command()
+def forge():
+    db.create_all()
+    name='Mr.li'
+    movies=[
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user=User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie=Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Forged successfully')
 @app.route('/')
 def index():  # put application's code here
     # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-    return render_template('index.html',name=name,movies=movies)
+    user=User.query.first()
+    movies=Movie.query.all()
+    return render_template('index.html',user=user,movies=movies)
 @app.route('/user/<name>')
 def user_page(name):
     return f'User:{name}'
