a10eeace22222fd822552feb7dc3ecae2d75ab88
隐藏玩家 <1131103319@qq.com>
2024-08-26 21:14:45 +0800

    add index page

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
-
+name='Mr.li'
+movies=[
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 @app.route('/')
-def hello_world():  # put application's code here
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():  # put application's code here
+    # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html',name=name,movies=movies)
 @app.route('/user/<name>')
 def user_page(name):
     return f'User:{name}'
-@app.route('/test')
-def test_url_for():
-    print(url_for("hello_world"))
-    print(url_for("user_page", name='a'))
-    print(url_for("user_page", name='b'))
-    print(url_for("test_url_for", num=2))
-    return 'test page'
+# @app.route('/test')
+# def test_url_for():
+#     print(url_for("hello_world"))
+#     print(url_for("user_page", name='a'))
+#     print(url_for("user_page", name='b'))
+#     print(url_for("test_url_for", num=2))
+#     return 'test page'
 
 if __name__ == '__main__':
     app.run(debug=True)
